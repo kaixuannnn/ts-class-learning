b89c5c9aba57d0e44f90be178eab3bb23eb19930
kaixuan <kaixuan@acepointer.sg>
2022-06-19 15:31:10 +0800

    creating classes

---
+++ src/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error();
+        this.balance += amount;
+    }
+}
